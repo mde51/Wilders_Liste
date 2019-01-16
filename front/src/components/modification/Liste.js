e5e0fdb412695d7d006f56ea940db59fff732fe4
Michel <descotesmichel@aol.com>
2019-01-16 14:18:50 +0100

    creation composant modification dans le front

---
+++ front/src/components/modification/Liste.js
@@ -0,0 +1,116 @@
+import React, { Component } from "react";
+import {
+  FormGroup,
+  Label,
+  Input,
+  Container,
+  Col,
+  Row,
+  Form,
+  Button
+} from "reactstrap";
+import axios from "axios";
+import Liste2 from "./Liste2";
+
+class Liste extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { wilders: [], email: "", liste: [], modification: [] };
+  }
+  componentDidMount() {
+    axios.get("http://localhost:5000/liste").then(response => {
+      console.log(response.data);
+      this.setState({ wilders: response.data });
+    });
+  }
+
+  // recherche des users via l'api par mails
+  rechercheMail(e) {
+    e.preventDefault();
+    axios.get(`/recherche/${this.state.email}`).then(response => {
+      this.setState({ liste: response.data });
+    });
+  }
+  // recherche des users via l'api par mail pour le supprimer
+  modificationMail(e) {
+    e.preventDefault();
+    axios.delete(`/modification/${this.state.email}`).then(response => {
+      this.setState({ delete: response.data });
+    });
+  }
+
+  // entrer le mail  dans le state via l'input
+  handleEmail(event) {
+    this.setState({ email: event.target.value });
+  }
+
+  render() {
+    return (
+      <Container>
+        <Row>
+          <Col>
+            <Form onSubmit={this.handleSubmit}>
+              <h2 className="text-secondary font-weight-bold mt-5" >Rechercher un Wilder par son email pour le supprimer</h2>
+              <FormGroup className="text-secondary font-weight-bold ">
+                <Label for="email">Email</Label>
+                <Input
+                  onChange={event => this.handleEmail(event)}
+                  value={this.state.email}
+                  type="email"
+                  name="email"
+                  id="exampleEmail"
+                  placeholder="mon@email.com"
+                />
+              </FormGroup>
+              {/* button avec evenement sur recherche mail */}
+              <Button
+                className="btn bg-primary float-right"
+                onClick={e => this.rechercheMail(e)}
+                type="submit"
+                value="submit"
+              >
+                Valider
+              </Button>
+            </Form>
+
+            {/* je test mon state ? si state = "quelque choses" , j'affiche <Row> FICHE </Row> : si non , j'affiche logo    {this.state.morning?<Page2/>:<Page3/>} */}
+            {this.state.email ?
+            <Row>
+              <Col className="d-flex justify-content-center mt-5">
+                <div className="text-center ">
+                  <h4 className="text-secondary font-weight-bold">
+                    Fiche du Wilder
+                  </h4>
+                  <Liste2 liste={this.state.liste} />
+                  <Button
+                    className="btn bg-primary float-right mt-2"
+                    onClick={e => this.modificationMail(e)}
+                    type="submit"
+                    value="submit"
+                  >
+                    Pour mofifier le Wilder Valider !!!
+                  </Button>
+                </div>
+              </Col>
+            </Row>
+            : 
+
+            <Row>
+              <Col sm="12" md={{ size: "auto", offset: 4 }} className="mt-1">
+                <img
+                  src="https://pbs.twimg.com/media/DwohpZQWkAEyHKS.jpg"
+                  alt="wcs"
+                  style={{ width: "25em" }}
+                  className="text-center mx-auto d-block mt-5"
+                />
+              </Col>
+            </Row>
+            }
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
+
+export default Liste;
---
+++ front/src/components/modification/Liste2.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Container, Col, Row } from "reactstrap";
+import Liste3 from "./Liste3";
+
+const Liste2 = props => {
+  return (
+    <Container>
+      <Row className="rounded p-1 bg-info text-white">
+        <Col>Nom</Col>
+        <Col>Prénom</Col>
+        <Col>email</Col>
+        {props.liste
+          .map((liste, index) => (
+            <Liste3 {...liste} />
+          ))}
+      </Row>
+    </Container>
+  );
+};
+export default Liste2;
